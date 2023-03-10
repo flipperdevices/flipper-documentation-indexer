9075fd3a68101710ce2794aa2dde0f0f90faa00a
DrunkBatya <drunkbatya.js@gmail.com>
2023-03-10 03:10:49 -0500

    Add parsing env vars

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ ADD src/ /app
 WORKDIR /app
 
 RUN python3 -m pip install -r requirements.txt
-CMD ["python3", " main.py"]
+CMD ["python3", "main.py"]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import uvicorn
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,26 +11,70 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
 def getArgs():
     parser = argparse.ArgumentParser(
         description="Returns JSON with S3 bucket directories list"
     )
     parser.add_argument(
-        "--access_key", required=True, help="S3 Access Key", metavar="KEY"
+        "--access_key",
+        envvar="ACCESS_KEY",
+        required=True,
+        help="S3 Access Key",
+        metavar="KEY",
+        action=EnvDefault,
     )
     parser.add_argument(
-        "--secret_key", required=True, help="S3 Secret Key", metavar="KEY"
+        "--secret_key",
+        envvar="SECRET_KEY",
+        required=True,
+        help="S3 Secret Key",
+        metavar="KEY",
+        action=EnvDefault,
     )
-    parser.add_argument("--endpoint", default="", help="S3 Endpoint URL", metavar="URL")
-    parser.add_argument("--region", required=True, help="S3 Region", metavar="REGION")
-    parser.add_argument("--bucket", default="", help="S3 Bucket", metavar="NAME")
+    parser.add_argument(
+        "--endpoint",
+        envvar="ENDPOINT",
+        default="",
+        help="S3 Endpoint URL",
+        metavar="URL",
+        action=EnvDefault,
+    )
+    parser.add_argument(
+        "--region",
+        envvar="REGION",
+        required=True,
+        help="S3 Region",
+        metavar="REGION",
+        action=EnvDefault,
+    )
+    parser.add_argument(
+        "--bucket",
+        envvar="BUCKET",
+        default="",
+        help="S3 Bucket",
+        metavar="NAME",
+        action=EnvDefault,
+    )
     return parser.parse_args()
 
 
 args = getArgs()
 
 
-def sortAndMoveDev(data: dict) -> dict:
+def sortAndMoveDev(data: list) -> list:
     data = sorted(data, reverse=True)
     data.insert(0, data.pop(data.index("dev")))
     return data
@@ -37,7 +82,7 @@ def sortAndMoveDev(data: dict) -> dict:
 
 def s3GetDirs(
     access_key: str, secret_key: str, endpoint: str, region: str, bucket: str
-) -> {}:
+) -> list:
     session = boto3.Session()
     client = session.client(
         service_name="s3",
