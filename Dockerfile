e1b8d5c801034757500bd2f71a20591b7381ab2f
DrunkBatya <drunkbatya.js@gmail.com>
2023-03-10 10:57:07 +0300

    Init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.11-alpine3.17
+
+ADD src/ /app
+WORKDIR /app
+
+RUN python3 -m pip install -r requirements.txt
+CMD ["python3", " main.py"]
---
+++ src/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import uvicorn
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+import boto3
+import argparse
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description="Returns JSON with S3 bucket directories list"
+    )
+    parser.add_argument(
+        "--access_key", required=True, help="S3 Access Key", metavar="KEY"
+    )
+    parser.add_argument(
+        "--secret_key", required=True, help="S3 Secret Key", metavar="KEY"
+    )
+    parser.add_argument("--endpoint", default="", help="S3 Endpoint URL", metavar="URL")
+    parser.add_argument("--region", required=True, help="S3 Region", metavar="REGION")
+    parser.add_argument("--bucket", default="", help="S3 Bucket", metavar="NAME")
+    return parser.parse_args()
+
+
+args = getArgs()
+
+
+def sortAndMoveDev(data: dict) -> dict:
+    data = sorted(data, reverse=True)
+    data.insert(0, data.pop(data.index("dev")))
+    return data
+
+
+def s3GetDirs(
+    access_key: str, secret_key: str, endpoint: str, region: str, bucket: str
+) -> {}:
+    session = boto3.Session()
+    client = session.client(
+        service_name="s3",
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        endpoint_url=endpoint,
+        region_name=region,
+    )
+    objects = client.list_objects(Bucket=bucket, Delimiter="/")
+    directories = [
+        name.get("Prefix").split("/")[0] for name in objects.get("CommonPrefixes")
+    ]
+    return sortAndMoveDev(directories)
+
+
+@app.get("/")
+async def root():
+    return s3GetDirs(
+        args.access_key, args.secret_key, args.endpoint, args.region, args.bucket
+    )
+
+
+def main():
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, workers=1)
+
+
+if __name__ == "__main__":
+    main()
